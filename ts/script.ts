0acf08a94c14a888093e0a04021f7506ffe2a6ae
JoyVanVlierberghe <joylindap.vanvlierberghe@hotmail.com>
2021-01-29 16:35:07 +0100

    update projects

--- ts/script.ts
+++ ts/script.ts
@@ -1,9 +1,5 @@
-//Get the button:
 this.mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction(): void {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -11,9 +7,11 @@ function scrollFunction(): void {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction(): void{
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+goToPeeCeeTrade(){
+  
 }
