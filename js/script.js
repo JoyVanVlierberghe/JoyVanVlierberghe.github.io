4b2e424f3d05242221326982ed7488f4820e45d0
JoyVanVlierberghe <joylindap.vanvlierberghe@hotmail.com>
2021-01-29 16:36:37 +0100

    function update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-//Get the button:
 this.mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction(); };
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -10,8 +8,7 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
